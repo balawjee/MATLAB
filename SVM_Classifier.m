e16702467c8d9037cdb5df3de45cb93e2e901d50
balawjee <balawjee@gmail.com>
2015-06-20 11:27:46 +0800

    Update SVM_Classifier.m

--- SVM_Classifier.m
+++ SVM_Classifier.m
@@ -1,5 +1,5 @@
 clc;
-javaaddpath('C:\Program Files (x86)\Weka-3-6\weka.jar');
+javaaddpath('Weka-3-6\weka.jar');
 
 import weka.core.Instances.*;
 import weka.core.converters.ConverterUtils.DataSource.*;
@@ -7,9 +7,9 @@ import weka.classifiers.Classifier.*;
 import weka.classifiers.Evaluation.*;
 
 %load SVM model
-Classifier cls = (Classifier) weka.core.SerializationHelper.read("C:/Users/Balajee/Desktop/FM Survey/Accelerometer Data/9 Sep/iPhone_model.model");
+Classifier cls = (Classifier) weka.core.SerializationHelper.read("iPhone_model.model");
 
-DataSource source = new DataSource("C:/Users/Balajee/Desktop/FM Survey/Accelerometer Data/9 Sep/features_iPhone_balanced.arff");
+DataSource source = new DataSource("features_iPhone_balanced.arff");
 Instances data = source.getDataSet();
 
 data.setClassIndex(data.numAttributes() - 1);
